2aa471df7c9804eb3a3e42db0ef3b0fbba45c078
hayden Tomas <hello@haydentomas.com>
2024-05-24 14:27:16 +0100

    Added icons to project feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,14 @@ import mdx from '@astrojs/mdx';
 import remarkGfm from 'remark-gfm';
 import remarkSmartypants from 'remark-smartypants';
 import rehypeExternalLinks from 'rehype-external-links';
+import icon from "astro-icon";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-template.netlify.app',
-  integrations: [mdx(), svelte(), tailwind()],
+  integrations: [mdx(), svelte(), tailwind(), icon() ],
   markdown: {
     shikiConfig: {
       theme: 'nord'
